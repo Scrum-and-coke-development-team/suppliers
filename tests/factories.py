e806437a7a23e3bdda7dc37dd992b3c7639c16b7
Alex Van Dorn <ajv7683@nyu.edu>
2022-03-09 13:21:07 -0500

    updated test models to match database schema

--- tests/factories.py
+++ tests/factories.py
@@ -17,19 +17,19 @@ Test Factory to make fake objects for testing
 """
 import factory
 from factory.fuzzy import FuzzyChoice
-from service.models import Pet, Gender
+from service.models import Supplier
 
 
 class PetFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+    """Creates fake suppliers that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = Pet
+        model = Supplier
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
-    category = FuzzyChoice(choices=["dog", "cat", "bird", "fish"])
+    name = factory.Faker("supplier_name")
+    category = FuzzyChoice(choices=["computing", "foods", "cosmetics", "drugs"])
     available = FuzzyChoice(choices=[True, False])
-    gender = FuzzyChoice(choices=[Gender.MALE, Gender.FEMALE, Gender.UNKNOWN])
+#    gender = FuzzyChoice(choices=[Gender.MALE, Gender.FEMALE, Gender.UNKNOWN])

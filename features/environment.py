e2ed254c9d934ab3fae478213c65be843d5a1919
Ethan Levy <ebl9622@stern.nyu.edu>
2022-05-06 18:10:36 -0400

    ibmcloud apps

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@
 Environment for Behave Testing
 """
 from os import getenv
+from behave import *
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
@@ -23,6 +24,7 @@ def before_all(context):
     context.WAIT_SECONDS = WAIT_SECONDS
     context.driver = webdriver.Chrome(options=options)
     context.driver.implicitly_wait(context.WAIT_SECONDS) # seconds
+    context.base_url = BASE_URL
 
     # context.driver.set_window_size(1200, 600)
 
@@ -35,3 +37,24 @@ def before_all(context):
 def after_all(context):
     """ Executed after all tests """
     context.driver.quit()
+
+def step_impl(context, text_string):
+    """ Get the value if a text field """
+    element = context.driver.find_element_by_id(element_id)
+    assert text_string in element.get_attribute('value')
+
+def step_impl(context, text_string):
+    """ Get the value if a text field """
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    expect(found).to_be(True)
+
+def step_impl(context, text_string):
+    """ Type a string into a text field """
+    element = context.driver.find_element_by_id(element_id)
+    element.clear()
+    element.send_keys(text_string)
